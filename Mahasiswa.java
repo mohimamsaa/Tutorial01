b04256084e445bee2235e6343c9136cba9a2e8de
Muhammad Imam Santosa <mohimamsa@gmail.com>
2018-09-07 16:39:53 +0700

    Inital Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,15 @@
+Public class Mahasiswa{
+	String nama;
+
+	public Mahasiswa(nama){
+		this.nama = nama;
+	}
+
+	public String getNama(){
+		return this.nama;
+	}
+
+	public setNama(nama){
+		this.nama = nama;
+	}
+}
